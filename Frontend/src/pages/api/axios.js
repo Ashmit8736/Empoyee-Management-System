d2a8323e579177faf54dfeb2da23017d948e08ed
Ashmit Singh <ashmit8736@gmail.com>
2026-03-02 23:48:36 +0530

    configure axios baseURL using env for production

--- Frontend/src/pages/api/axios.js
+++ Frontend/src/pages/api/axios.js
@@ -1,18 +1,42 @@
+// import axios from "axios";
+
+// const api = axios.create({ baseURL: "http://localhost:5001/api",
+// headers: { "Content-Type": "application/json" }, });
+
+// api.interceptors.request.use((config) => {
+//   const adminToken = localStorage.getItem("adminToken");
+//   const employeeToken = localStorage.getItem("employeeToken");
+
+//   // 🔥 ADMIN LOGGED IN → ADMIN TOKEN ALWAYS PREFERRED
+//   if (adminToken) {
+//     config.headers.Authorization = `Bearer ${adminToken}`;
+//   }
+//   // ELSE EMPLOYEE
+//   else if (employeeToken) {
+//     config.headers.Authorization = `Bearer ${employeeToken}`;
+//   }
+
+//   return config;
+// });
+
+// export default api;
+
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5001/api",
-headers: { "Content-Type": "application/json" }, });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 api.interceptors.request.use((config) => {
   const adminToken = localStorage.getItem("adminToken");
   const employeeToken = localStorage.getItem("employeeToken");
 
-  // 🔥 ADMIN LOGGED IN → ADMIN TOKEN ALWAYS PREFERRED
   if (adminToken) {
     config.headers.Authorization = `Bearer ${adminToken}`;
-  }
-  // ELSE EMPLOYEE
-  else if (employeeToken) {
+  } else if (employeeToken) {
     config.headers.Authorization = `Bearer ${employeeToken}`;
   }
 
@@ -22,32 +46,3 @@ api.interceptors.request.use((config) => {
 export default api;
 
 
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// api.interceptors.request.use(
-//   (config) => {
-//     const adminToken = localStorage.getItem("adminToken");
-//     const employeeToken = localStorage.getItem("employeeToken");
-
-//     // 🔥 ADMIN LOGGED IN → ADMIN TOKEN ALWAYS PREFERRED
-//     if (adminToken) {
-//       config.headers.Authorization = `Bearer ${adminToken}`;
-//     }
-//     // ELSE EMPLOYEE
-//     else if (employeeToken) {
-//       config.headers.Authorization = `Bearer ${employeeToken}`;
-//     }
-
-//     return config;
-//   },
-//   (error) => Promise.reject(error)  
-// );
-
-// export default api;
